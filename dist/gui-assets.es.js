e68ff84f7ddad4320cb8c6c67da63e069f03d938
Kirill Galushko <yoursgk@mail.ru>
2024-11-26 01:34:23 +0600

    Release 1.0.0

--- dist/gui-assets.es.js
+++ dist/gui-assets.es.js
@@ -1,64 +1,64 @@
-import { _ as e } from "./logo/AndroidLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as e } from "./logo/AngularLogo.vue_vue_type_script_setup_true_lang.ts.js";
 import { _ as s } from "./logo/ApachecordovaLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as p } from "./logo/AngularLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as p } from "./logo/AndroidLogo.vue_vue_type_script_setup_true_lang.ts.js";
 import { _ as x } from "./logo/CrystalnixLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as L } from "./logo/ElectronLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ } from "./logo/CircleciLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as L } from "./logo/CssLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ } from "./logo/ElectronLogo.vue_vue_type_script_setup_true_lang.ts.js";
 import { _ as c } from "./logo/FigmaLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as n } from "./logo/CssLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as n } from "./logo/CircleciLogo.vue_vue_type_script_setup_true_lang.ts.js";
 import { _ as l } from "./logo/GitLogo.vue_vue_type_script_setup_true_lang.ts.js";
 import { _ as b } from "./logo/GithubLogo.vue_vue_type_script_setup_true_lang.ts.js";
 import { _ as C } from "./logo/GraphqlLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as G } from "./logo/JavascriptLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as R } from "./logo/HhruLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as j } from "./logo/JestLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as v } from "./logo/JiraLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as N } from "./logo/LessLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as q } from "./logo/NextjsLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as F } from "./logo/HtmlLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as S } from "./logo/NodejsLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as W } from "./logo/ReduxLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as z } from "./logo/PythonLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as D } from "./logo/ReactLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as K } from "./logo/RonasitLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as G } from "./logo/HhruLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as R } from "./logo/HtmlLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as j } from "./logo/JavascriptLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as v } from "./logo/JestLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as N } from "./logo/JiraLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as q } from "./logo/LessLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as F } from "./logo/NextjsLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as S } from "./logo/PythonLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as W } from "./logo/ReactLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as z } from "./logo/ReduxLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as D } from "./logo/RonasitLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as K } from "./logo/NodejsLogo.vue_vue_type_script_setup_true_lang.ts.js";
 import { _ as O } from "./logo/StorybookLogo.vue_vue_type_script_setup_true_lang.ts.js";
 import { _ as X } from "./logo/TermiusLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as Z } from "./logo/UbuntuLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as oo } from "./logo/TypescriptLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as eo } from "./logo/ViteLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as Z } from "./logo/TypescriptLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as oo } from "./logo/ViteLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as eo } from "./logo/UbuntuLogo.vue_vue_type_script_setup_true_lang.ts.js";
 import { _ as so } from "./logo/VsCodeLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as po } from "./logo/WebpackLogo.vue_vue_type_script_setup_true_lang.ts.js";
-import { _ as xo } from "./logo/VueLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as po } from "./logo/VueLogo.vue_vue_type_script_setup_true_lang.ts.js";
+import { _ as xo } from "./logo/WebpackLogo.vue_vue_type_script_setup_true_lang.ts.js";
 export {
-  e as AndroidLogo,
-  p as AngularLogo,
+  p as AndroidLogo,
+  e as AngularLogo,
   s as ApachecordovaLogo,
-  _ as CircleciLogo,
+  n as CircleciLogo,
   x as CrystalnixLogo,
-  n as CssLogo,
-  L as ElectronLogo,
+  L as CssLogo,
+  _ as ElectronLogo,
   c as FigmaLogo,
   l as GitLogo,
   b as GithubLogo,
   C as GraphqlLogo,
-  R as HhruLogo,
-  F as HtmlLogo,
-  G as JavascriptLogo,
-  j as JestLogo,
-  v as JiraLogo,
-  N as LessLogo,
-  q as NextjsLogo,
-  S as NodejsLogo,
-  z as PythonLogo,
-  D as ReactLogo,
-  W as ReduxLogo,
-  K as RonasitLogo,
+  G as HhruLogo,
+  R as HtmlLogo,
+  j as JavascriptLogo,
+  v as JestLogo,
+  N as JiraLogo,
+  q as LessLogo,
+  F as NextjsLogo,
+  K as NodejsLogo,
+  S as PythonLogo,
+  W as ReactLogo,
+  z as ReduxLogo,
+  D as RonasitLogo,
   O as StorybookLogo,
   X as TermiusLogo,
-  oo as TypescriptLogo,
-  Z as UbuntuLogo,
-  eo as ViteLogo,
+  Z as TypescriptLogo,
+  eo as UbuntuLogo,
+  oo as ViteLogo,
   so as VsCodeLogo,
-  xo as VueLogo,
-  po as WebpackLogo
+  po as VueLogo,
+  xo as WebpackLogo
 };
